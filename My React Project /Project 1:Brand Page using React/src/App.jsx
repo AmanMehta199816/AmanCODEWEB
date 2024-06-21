abebdc457575cbcb9febe20f06516f70afab74af
Aman Mehta <96304523+AmanMehta199816@users.noreply.github.com>
2024-06-21 08:02:36 +0000

    setting.json

---
+++ My React Project /Project 1:Brand Page using React/src/App.jsx
@@ -0,0 +1,21 @@
+const App = () => {
+ return <div>
+  <Nav>
+    <div className=" Aman logo">
+      <img src="C:\Users\aa\Downloads\Nike Logo PNG Transparent & SVG Vector - Freebie Supply.jpeg" alt="logo" />
+    </div>
+
+   <ul>
+    <li href="#"> Menu </li>
+    <li href="#"> About </li>
+    <li href="#"> Location </li>
+    <li href="#"> Contact </li>
+    
+    </ul> 
+
+    <button>login</button>
+  </Nav>
+ </div>;
+
+};
+export default App; 
